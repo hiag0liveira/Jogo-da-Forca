f154767da98d7e18233fac62d21571afb0788c53
Hiago de Oliveira Mendes <99771272+hiag0liveira@users.noreply.github.com>
2023-04-06 11:41:34 -0300

    Delete src/main/java/br/edu/iff/dominio/boneco directory

--- src/main/java/br/edu/iff/dominio/boneco/Boneco.java
+++
@@ -1,5 +0,0 @@
-package br.edu.iff.dominio.boneco;
-
-public interface Boneco {
-    void exibir(Object contexto, int partes);
-}
--- src/main/java/br/edu/iff/dominio/boneco/BonecoFactory.java
+++
@@ -1,5 +0,0 @@
-package br.edu.iff.dominio.boneco;
-
-public interface BonecoFactory {
-    Boneco getBoneco();
-}
